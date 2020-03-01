a41e3d69c9926ca5383cb3b5902970f8ab597c73
David Fagan <david.fagan@robotsandpencils.com>
2020-03-01 14:00:11 -0500

    webpack documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -41,60 +41,9 @@ module.exports = {
       'module.rules.use.oneOf': 'append'
     })(config, newLoaders)
 
-    // config.module.rules = config.module.rules.map(obj => {
-    //   if (!obj.oneOf) {
-    //     return obj
-    //   } else {
-    //     return {
-    //       oneOf: obj.oneOf.filter(o => {
-    //         if (!o.test) {
-    //           console.log('in default')
-    //           return true
-    //         } else if (o.test.toString() === /\.(scss|sass)$/.toString()) {
-    //           console.log('in normal')
-    //           return false
-    //         } else if (o.test.toString() === /\.module\.(scss|sass)$/.toString()) {
-    //           console.log('in modules')
-    //           return false
-    //         } else {
-    //           console.log('in catch')
-    //           return true
-    //         }
-    //       }).concat([{
-    //         test: /\.module\.(scss|sass)$/,
-    //         use: [
-    //           {
-    //             loader: 'style-loader',
-    //             options: { injectType: 'lazyStyleTag' }
-    //           },
-    //           {
-    //             loader: 'css-loader',
-    //             options: { importLoaders: 4 }
-    //           },
-    //           {
-    //             loader: 'postcss-loader',
-    //             options: { plugins: () => [require('autoprefixer')] }
-    //           },
-    //           {
-    //             loader: 'resolve-url-loader',
-    //             options: { sourceMap: false }
-    //           },
-    //           {
-    //             loader: 'sass-loader',
-    //             options: { sourceMap: true }
-    //           },
-    //           {
-    //             loader: 'sass-resources-loader',
-    //             options: { resources: './src/lib/styles/tokens/*.scss' }
-    //           }
-    //         ]
-    //       }])
-    //     }
-    //   }
-    // })
-
     combinedConfig.resolve.alias = Object.assign({}, combinedConfig.resolve.alias, { themes: path.resolve(__dirname, '../src/lib/styles/themes')})
 
+    // the below is a hefty effort to simply add { injectType: 'lazyStyleTag' } to the existing style-loader config
     const newRules = combinedConfig.module.rules.map(obj => {
       if (obj.test) {
         return obj
