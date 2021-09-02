2fd7bbff1335794545544a0e35bb9c9e9d74ec96
Corey Funk <coreyafunk@gmail.com>
2021-09-02 11:49:15 -0500

    Add checkbox to demonstrate controlled tab functionality

--- src/stories/Tabs.stories.js
+++ src/stories/Tabs.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { useState } from '@storybook/client-api'
+import { useState, useEffect, useMemo } from '@storybook/client-api'
 import Tabs, { Tabs as TabsComponent } from '../core/components/tabs/Tabs'
+import FormCheckbox from '../core/components/formCheckbox/FormCheckbox'
 
 export default {
   title: 'Robits/Tabs',
@@ -8,11 +9,21 @@ export default {
 }
 
 export const Normal = ({ themeName }) => {
-  const tabOptions = [
-    { label: 'Tab 1', enabled: true },
-    { label: 'Tab 2', enabled: true }
-  ]
+  const tabOptions = useMemo(
+    () => [
+      { label: 'Tab 1', enabled: true },
+      { label: 'Tab 2', enabled: true }
+    ],
+    []
+  )
+
   const [activeTab, setActiveTab] = useState(tabOptions[0].label)
+  const [isControlled, setIsControlled] = useState(false)
+
+  // Reinitialize tab state
+  useEffect(() => {
+    setActiveTab(tabOptions[0].label)
+  }, [isControlled, tabOptions])
 
   const handleTabChange = tab => {
     if (tab) {
@@ -22,9 +33,20 @@ export const Normal = ({ themeName }) => {
 
   return (
     <>
+      <FormCheckbox
+        themeName={themeName}
+        id='is-controlled-checkbox'
+        toggle
+        onChange={() => {
+          setIsControlled(!isControlled)
+        }}
+        checked={isControlled}>
+        Is Controlled
+      </FormCheckbox>
       <Tabs
         themeName={themeName}
         defaultActiveTab={activeTab}
+        activeTab={isControlled ? activeTab : undefined}
         options={tabOptions}
         onChangeCallback={handleTabChange}
       />
